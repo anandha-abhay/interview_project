1a574bc1030f9afad6c0b184ccc38c22603774d1
Matt Abhay <matt@partyslate.com>
2019-08-12 03:38:58 -0500

    [CH-8] Display most popular year queries
    The CEO wants moar data.  A couple more feature requests from our logs
    and I am going to point him to our data warehouse so another team can
    build these dashboards.
    
    Resolves CH-8

--- app/controllers/query_logs_controller.rb
+++ app/controllers/query_logs_controller.rb
@@ -4,6 +4,7 @@ class QueryLogsController < ApplicationController
   # GET /query_logs.json
   def index
     @query_logs = QueryLog.all
+    @popular_queries = QueryLog.known_year_query_counts
   end
 
 end
